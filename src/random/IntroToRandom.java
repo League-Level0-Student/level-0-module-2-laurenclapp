ecc75d109ebf61d211db9a777252f98ebbf364ff
clapp.lauren <clapp.lauren@SEMS-259-10.SEMS259.LAB>
2018-01-24 14:56:58 -0800

    random

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -16,12 +16,16 @@ public class IntroToRandom {
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
+		Random rand = new Random();
+		r= rand.nextInt(101);
 
 		//3. Limit the random number between 0 and 100
 
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
+		r= rand.nextInt(50)+25;
 
 		//5. Challenge: Limit the random number between -222 and 88
+		r=rand.nextInt(88-(-222))+-222;
 
 
 		
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -8,20 +8,39 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class Validation {
+	
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			
+		
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
 		
-		int randomNumber = new Random().nextInt(5);
+		int randomChoice = new Random().nextInt(5);
 		
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
 		
-		System.out.println(randomNumber);
+	
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+	
+
+		// 2. Fix the code below so that it displays the correct image
+		if (randomChoice == 0) {
+			JOptionPane.showMessageDialog(null, "you have really nice hair");
+		} else if (randomChoice == 1) {
+			JOptionPane.showMessageDialog(null, "nice face");
+		} else if (randomChoice == 2) {
+			JOptionPane.showMessageDialog(null, "i love u");
+		} else if (randomChoice == 3) {
+			JOptionPane.showMessageDialog(null, "cool shirt");
+		} else if (randomChoice == 4) {
+			JOptionPane.showMessageDialog( null, "you are awesome");
+		}
 
 		// 4. Repeat all the code above 10 times
 		
+		
 		// 5. Find someone to test out your program. They will like it :)
 	}
-}
+}}
